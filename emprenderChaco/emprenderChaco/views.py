4e8143de7310aae81667a79f23de63da845413f3
IvaaNZ <husar2k16@gmail.com>
2023-08-03 13:36:19 -0300

    Actualizado 99%

--- emprenderChaco/emprenderChaco/views.py
+++ emprenderChaco/emprenderChaco/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
-
-
+from apps.noticias.models import Noticia
 
 def index(request):
-    return render(request, 'index.html', {'is_index': True})
+    noticias = Noticia.objects.order_by('-creado')[:3]
+    return render(request, 'index.html', {'noticias': noticias})
+
+
+
+#def index(request):
+ #   return render(request, 'index.html', {'is_index': True})
 
 def nosotros(request):
     return render(request, 'nosotros/nosotros.html')
