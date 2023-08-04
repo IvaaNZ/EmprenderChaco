95e5905c05c49a8e6969c919e24d6608196117c6
IvaaNZ <husar2k16@gmail.com>
2023-08-04 09:04:35 -0300

    Fixeado error de inicio de sesion

--- emprenderChaco/apps/usuarios/forms.py
+++ emprenderChaco/apps/usuarios/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
+class MyLoginForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': "Ingrese Usuario y contraseña Correcta.",
+    }
 
 
 class RegistroForm(UserCreationForm):
--- emprenderChaco/apps/usuarios/views.py
+++ emprenderChaco/apps/usuarios/views.py
@@ -2,7 +2,23 @@
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import RegistroForm
-# Create your views here.
+
+from django.shortcuts import render, redirect
+
+from .forms import MyLoginForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = MyLoginForm(request, data=request.POST)
+        if form.is_valid():
+            from django.contrib.auth import login
+            user = form.get_user()
+            login(request, user)
+            return redirect('nombre_de_la_url_de_inicio')  # Reemplaza 'nombre_de_la_url_de_inicio' con la URL a la que deseas redirigir al usuario después de iniciar sesión correctamente.
+    else:
+        form = MyLoginForm()
+    
+    return render(request, 'login.html', {'form': form})
 
 
 
